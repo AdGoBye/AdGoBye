88d91430d3c4bc559d7a76219f82f423c88b7f29
Regalia <regalia@tutanota.de>
2023-12-03 09:28:46 +0100

    fix: Forgot to use context logger for the no blocklist log

--- AdGoBye/Program.cs
+++ AdGoBye/Program.cs
@@ -29,7 +29,7 @@ foreach (var plugin in PluginLoader.LoadedPlugins)
         logger.Information("Responsible for {IDs}", plugin.Instance.ResponsibleForContentIds());
 }
 
-if (Blocklist.Blocks.Count == 0) Log.Information("No blocklist has been loaded, is this intentional?");
+if (Blocklist.Blocks.Count == 0) logger.Information("No blocklist has been loaded, is this intentional?");
 logger.Information("Loaded blocks for {blockCount} worlds and indexed {indexCount} pieces of content",
     Blocklist.Blocks.Count, Indexer.Index.Count);
 
