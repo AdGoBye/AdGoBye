268258d057853988f22d9fb7e8c491d861540d3a
Regalia <regalia@tutanota.de>
2024-01-03 05:02:15 +0100

    fix: Reintroduce main code

--- AdGoBye/Program.cs
+++ AdGoBye/Program.cs
@@ -1,6 +1,10 @@
 using AdGoBye;
+using AdGoBye.Plugins;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
 using Serilog;
 using Serilog.Core;
 using Serilog.Events;
@@ -14,7 +18,8 @@ var levelSwitch = new LoggingLevelSwitch
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.ControlledBy(levelSwitch)
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
     .Enrich.FromLogContext()
     .WriteTo.Console(new ExpressionTemplate(
         "[{@t:HH:mm:ss} {@l:u3} {Coalesce(SourceContext,'<none>')}] {@m}\n{@x}",
@@ -22,7 +27,6 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 var builder = Host.CreateDefaultBuilder(args)
-    
     .ConfigureServices(collection =>
     {
         collection.AddDbContext<IndexContext>();
@@ -34,6 +38,32 @@ var builder = Host.CreateDefaultBuilder(args)
 
 using var host = builder.Build();
 var awa = host.Services.GetRequiredService<Indexer>();
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+await using var db = host.Services.GetRequiredService<IndexContext>();
+db.Database.Migrate();
 awa.ManageIndex();
 
+PluginLoader.LoadPlugins();
+foreach (var plugin in PluginLoader.LoadedPlugins)
+{
+    logger.LogInformation("Plugin {Name} ({Maintainer}) v{Version} is loaded.", plugin.Name, plugin.Maintainer,
+        plugin.Version);
+    logger.LogInformation("Plugin type: {Type}", plugin.Instance.PluginType());
+
+    if (plugin.Instance.PluginType() == EPluginType.ContentSpecific)
+        logger.Log(LogLevel.Information,"Responsible for {IDs}", plugin.Instance.ResponsibleForContentIds());
+}
+
+if (Blocklist.Blocks.Count == 0) logger.LogInformation("No blocklist has been loaded, is this intentional?");
+logger.LogInformation("Loaded blocks for {blockCount} worlds and indexed {indexCount} pieces of content",
+    Blocklist.Blocks.Count, db.Content.Count());
+
+foreach (var content in db.Content.Include(content => content.VersionMeta))
+{
+    if (content.Type != ContentType.World) continue;
+    logger.LogInformation("Processing {ID} ({director})", content.Id, content.VersionMeta.Path);
+    awa.PatchContent(content);
+}
+
+db.SaveChanges();
 host.Run();
