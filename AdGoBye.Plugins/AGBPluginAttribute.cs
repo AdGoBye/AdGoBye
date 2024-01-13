f7d41ec946c47dfee5b13e3f74cdf575edd6cd88
Regalia <regalia@tutanota.de>
2024-01-13 17:11:29 +0100

    refactor: Use primary constructor for AgbPluginAttribute

--- AdGoBye.Plugins/AGBPluginAttribute.cs
+++ AdGoBye.Plugins/AGBPluginAttribute.cs
@@ -1,17 +1,10 @@
 ﻿namespace AdGoBye.Plugins;
 
-public class AgbPluginAttribute : Attribute
+public class AgbPluginAttribute(string name, string maintainer, string version, Type instance)
+    : Attribute
 {
-    public Type Instance;
-    public string Maintainer;
-    public string Name;
-    public string Version;
-
-    public AgbPluginAttribute(string name, string maintainer, string version, Type instance)
-    {
-        Name = name;
-        Maintainer = maintainer;
-        Version = version;
-        Instance = instance;
-    }
+    public Type Instance = instance;
+    public string Maintainer = maintainer;
+    public string Name = name;
+    public string Version = version;
 }
