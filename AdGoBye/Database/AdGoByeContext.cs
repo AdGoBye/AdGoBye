b19aabfa47be5b46edb346fff7ac113dc7b28997
Regalia <regalia@tutanota.de>
2024-07-07 01:39:09 +0200

    chore: Set EnableSensitiveDataLogging for DbContext
    We don't actually store sensitive data so logging shouldn't be an issue.

--- AdGoBye/Database/AdGoByeContext.cs
+++ AdGoBye/Database/AdGoByeContext.cs
@@ -4,12 +4,13 @@ namespace AdGoBye.Database;
 
 public sealed class AdGoByeContext : DbContext
 {
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        options.UseSqlite("Data Source=database.db");
-    }
-
     public DbSet<Content> Content { get; set; }
     public DbSet<Content.ContentVersionMeta> ContentVersionMetas { get; set; }
     public DbSet<Blocklist.NetworkBlocklist> NetworkBlocklists { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite("Data Source=database.db");
+        options.EnableSensitiveDataLogging();
+    }
 }
